135bddb928e1cb4919629f0303d88f8f08b2b72b
FilbertSM <filbertsm7@gmail.com>
2026-04-29 10:29:06 +0700

    feat: SEO Optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,40 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: "Soto Anggut | The Authentic Makassar Experience",
-  description: "A high-end, cinematic landing page for Soto Anggut.",
+  title: "Soto Ayam Kampung Legend Anggut | Kuliner Balikpapan",
+  description: "Nikmati soto ayam kampung terdekat dan paling otentik di Balikpapan. Kuliner legendaris dengan resep asli Makassar.",
+  openGraph: {
+    title: "Soto Ayam Kampung Legend Anggut",
+    description: "Kuliner otentik legendaris di Balikpapan. Pesan sekarang atau kunjungi kedai kami.",
+    url: "https://maps.app.goo.gl/GYDn5LnSerBnbts66",
+    siteName: "Soto Anggut",
+    locale: "id_ID",
+    type: "website",
+  },
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Restaurant",
+  "name": "Soto Ayam Kampung Legend Anggut",
+  "image": "https://maps.app.goo.gl/GYDn5LnSerBnbts66",
+  "@id": "https://maps.app.goo.gl/GYDn5LnSerBnbts66",
+  "url": "https://maps.app.goo.gl/GYDn5LnSerBnbts66",
+  "telephone": "+6289678243688",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Jl. Pemuda II No.60, Damai",
+    "addressLocality": "Kec. Balikpapan Kota, Kota Balikpapan",
+    "addressRegion": "Kalimantan Timur",
+    "postalCode": "76114",
+    "addressCountry": "ID"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": -1.2695166,
+    "longitude": 116.8574575
+  },
+  "servesCuisine": "Indonesian, Soto Ayam Kampung"
 };
 
 export default function RootLayout({
@@ -25,10 +57,14 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="id"
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <SmoothScrolling>{children}</SmoothScrolling>
       </body>
     </html>
