c67107cff012d6b2774d3016d5858e419e955ee2
FilbertSM <filbertsm7@gmail.com>
2026-04-29 10:41:07 +0700

    fix: configure basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Tells Next.js to create a static HTML/CSS/JS build
+  basePath: '/Soto-Ayam-Kampung-Legend-Anggut', // Required for GitHub Pages subpath
 };
 
 export default nextConfig;
