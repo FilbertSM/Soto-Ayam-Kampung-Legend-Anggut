a210a50bf1d6ac60f9db312df5f36f3b3501f3be
FilbertSM <filbertsm7@gmail.com>
2026-05-01 17:13:05 +0700

    fix: restore default next config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to create a static HTML/CSS/JS build
-  basePath: isProd ? '/Soto-Ayam-Kampung-Legend-Anggut' : '', // Required for GitHub Pages subpath
+  /* config options here */
 };
 
 export default nextConfig;
